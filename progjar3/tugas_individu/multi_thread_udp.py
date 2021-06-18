24c672bb029d8c53459e5ac4f901dcca598e01dd
chintya dewi <chintyadewi@chintya.local>
2021-06-18 10:48:08 +0800

    done_t3

---
+++ progjar3/tugas_individu/multi_thread_udp.py
@@ -0,0 +1,40 @@
+from library import download_gambar,get_url_list,kirim_gambar
+import time
+import datetime
+import threading
+
+def kirim_semua():
+    texec = dict()
+    urls = get_url_list()
+    temp = 0
+    catat_awal = datetime.datetime.now()
+    for k in urls:
+        download_gambar(urls[k], k)
+        print(f"mendownload {urls[k]}")
+        waktu = time.time()
+        #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multithread
+        UDP_IP_ADDRESS = "192.168.122.235"
+        UDP_IP_ADDRESS2 = "192.168.122.107"
+        if temp == 0:
+            texec[k] = threading.Thread(target=kirim_gambar, args=(UDP_IP_ADDRESS, 5050, f"{k}.jpg"))
+            print('masuk server 1')
+            temp = temp + 1
+        elif temp == 1:
+            print('masuk server 2')
+            texec[k] = threading.Thread(target=kirim_gambar, args=(UDP_IP_ADDRESS2, 5050, f"{k}.jpg"))
+        # texec[k] = threading.Thread(target=download_gambar, args=(urls[k],))
+        texec[k].start()
+
+    #setelah menyelesaikan tugasnya, dikembalikan ke main thread dengan join
+    for k in urls:
+        texec[k].join()
+
+    catat_akhir = datetime.datetime.now()
+    selesai = catat_akhir - catat_awal
+    print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
+
+
+#fungsi download_gambar akan dijalankan secara multithreading
+
+if __name__=='__main__':
+    kirim_semua()
